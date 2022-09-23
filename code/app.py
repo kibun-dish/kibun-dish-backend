a24127f96b4ca7bdb5a87ecc34856f822c1eaed7
Junpei Nitami <xintainjianjunpei@gmail.com>
2022-09-23 16:33:39 +0900

    rooting

--- code/app.py
+++ code/app.py
@@ -1,10 +1,11 @@
+from operator import methodcaller
 import os
 from unicodedata import category
 from weakref import ref 
 from xml.etree.ElementPath import prepare_parent
 from flask import Flask, render_template, jsonify
 from flask import request, redirect, url_for
-
+import json
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import column
 from flask_cors import CORS
@@ -21,11 +22,30 @@ db = SQLAlchemy(app)
 
 CORS(app)
 
+# @app.route('/', defaults={'path':''},method=["GET"])
+# def index():
+#     return json.dumps()
 
-@app.route('/', defaults={'path':''})
-@app.route('/<path:path>')
-def index(path):
-    return render_template('index.html') 
+@app.route('/foods', defaults={'path':''},method=["GET"])
+def show_foods():
+    return json.dumps()
+
+@app.route('/feels', defaults={'path':''},method=["GET"])
+def show_feels():
+    return json.dumps()
+
+@app.route('/relations', defaults={'path':''},method=["GET"])
+def show_relations():
+    return json.dumps()
+
+@app.route('/create/relations', defaults={'path':''},method=["POST"])
+def show_relations():
+    # create feels and foods
+    return "200"
+
+# @app.route('/<path:path>')
+# def index(path):
+#     return render_template('index.html') 
 
 db.init(app)
 if __name__ == '__main__':
